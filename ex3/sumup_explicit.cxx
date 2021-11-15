4add107632bb3b74b5ebc9455557bd47cc731cfc
Holger Schulz <holger.schulz@durham.ac.uk>
2021-11-15 17:10:42 +0000

    cosmetics

--- ex3/sumup_explicit.cxx
+++ ex3/sumup_explicit.cxx
@@ -1,13 +1,17 @@
 #include <cstdio>
 #include "omp.h"
 
+unsigned long crosscheck(unsigned long N)
+{
+    return N*(N+1)/2;
+}
+
 int main()
 {
     unsigned long N = 1000000000;
-
     unsigned long SUMS[4] = {0,0,0,0};
 
-#pragma omp parallel for
+    #pragma omp parallel for default(none)
     for (unsigned long i=1;i<N+1;i++)
     {
         SUMS[omp_get_thread_num()]+=i;
@@ -15,7 +19,8 @@ int main()
 
     unsigned long result = SUMS[0] + SUMS[1] + SUMS[2] + SUMS[3];
 
-    printf("Result: %lu\n", result);
+    printf("Result:    %lu\n", result);
+    printf("Should be: %lu\n", crosscheck(N));
 
     return 0;
 }
